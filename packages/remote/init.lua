02fa65dbdadbf8dbcc895ee9e4a2708f3cf08221
Stephen Leitnick <sleitnick@gmail.com>
2021-10-08 17:02:35 -0400

    Add Remote

---
+++ packages/remote/init.lua
@@ -0,0 +1,11 @@
+-- Remote
+-- Stephen Leitnick
+-- November 08, 2021
+
+
+return {
+	RemoteSignal = require(script.RemoteSignal);
+	RemoteProperty = require(script.RemoteProperty);
+	ClientRemoteSignal = require(script.ClientRemoteSignal);
+	ClientRemoteProperty = require(script.ClientRemoteProperty);
+}
