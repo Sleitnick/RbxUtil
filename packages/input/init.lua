048abf88447cbfbae962899e103e27e773edc1a8
Stephen Leitnick <sleitnick@gmail.com>
2021-10-11 00:36:47 -0400

    Docs update

--- packages/input/init.lua
+++ packages/input/init.lua
@@ -4,12 +4,28 @@
 
 --[=[
 	@class Input
-	Test
-]=]
 
-return {
-	Preferred = require(script.Preferred);
+	The Input module provides access to various user input classes.
+
+	- [PreferredInput](/api/PreferredInput)
+	- [Mouse](/api/Mouse)
+	- [Keyboard](/api/Keyboard)
+	- [Touch](/api/Touch)
+
+	```lua
+	local Input = require(packages.Input)
+
+	local PreferredInput = Input.PreferredInput
+	local Mouse = Input.Mouse
+	local Keyboard = Input.Keyboard
+	local Touch = Input.Touch
+	```
+]=]
+local Input = {
+	PreferredInput = require(script.PreferredInput);
 	Mouse = require(script.Mouse);
 	Keyboard = require(script.Keyboard);
 	Touch = require(script.Touch);
 }
+
+return Input
--- packages/remote/init.lua
+++ packages/remote/init.lua
@@ -3,9 +3,30 @@
 -- November 08, 2021
 
 
-return {
+--[=[
+	@class Remote
+
+	The Remote module provides access to various remote communication classes.
+
+	- [RemoteSignal](/api/RemoteSignal)
+	- [RemoteProperty](/api/RemoteProperty)
+	- [ClientRemoteSignal](/api/ClientRemoteSignal)
+	- [ClientRemoteProperty](/api/ClientRemoteProperty)
+
+	```lua
+	local Remote = require(packages.Remote)
+	
+	local RemoteSignal = Remote.RemoteSignal
+	local RemoteProperty = Remote.RemoteProperty
+	local ClientRemoteSignal = Remote.ClientRemoteSignal
+	local ClientRemoteProperty = Remote.ClientRemoteProperty
+	```
+]=]
+local Remote = {
 	RemoteSignal = require(script.RemoteSignal);
 	RemoteProperty = require(script.RemoteProperty);
 	ClientRemoteSignal = require(script.ClientRemoteSignal);
 	ClientRemoteProperty = require(script.ClientRemoteProperty);
 }
+
+return Remote
