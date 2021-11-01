bb3729cdb82644f162fedbdfff9a4e9847bd4d2c
Stephen Leitnick <sleitnick@gmail.com>
2021-11-01 18:57:40 -0400

    CI

--- test/RunTests.server.lua
+++ test/RunTests.server.lua
@@ -1,8 +1,9 @@
 print("Running unit tests...")
+local TestService = game:GetService("TestService")
 local tests = {}
-for _,testFolder in ipairs(script.Parent.modules:GetChildren()) do
+for _,testFolder in ipairs(TestService.modules:GetChildren()) do
 	local name = testFolder.Name:match("(.+)_test$")
 	local module = testFolder[name]
 	table.insert(tests, module)
 end
-require(script.Parent.Packages.TestEZ).TestBootstrap:run(tests)
+require(TestService.Packages.TestEZ).TestBootstrap:run(tests)
