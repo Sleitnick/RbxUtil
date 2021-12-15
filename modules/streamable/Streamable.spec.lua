874f05b8b28efcc5ddc728a7fde918ae5dfee2ba
Stephen Leitnick <sleitnick@gmail.com>
2021-12-14 22:34:39 -0500

    Streamable primary part

--- modules/streamable/Streamable.spec.lua
+++ modules/streamable/Streamable.spec.lua
@@ -3,6 +3,7 @@ return function()
 	local Streamable = require(script.Parent.Streamable)
 
 	local instanceFolder
+	local instanceModel
 
 	local function CreateInstance(name)
 		local folder = Instance.new("Folder")
@@ -12,19 +13,33 @@ return function()
 		return folder
 	end
 
+	local function CreatePrimary()
+		local primary = Instance.new("Part")
+		primary.Anchored = true
+		primary.Parent = instanceModel
+		instanceModel.PrimaryPart = primary
+		return primary
+	end
+
 	beforeAll(function()
 		instanceFolder = Instance.new("Folder")
-		instanceFolder.Name = "KnitTest"
+		instanceFolder.Name = "KnitTestFolder"
 		instanceFolder.Archivable = false
 		instanceFolder.Parent = workspace
+		instanceModel = Instance.new("Model")
+		instanceModel.Name = "KnitTestModel"
+		instanceModel.Archivable = false
+		instanceModel.Parent = workspace
 	end)
 
 	afterEach(function()
 		instanceFolder:ClearAllChildren()
+		instanceModel:ClearAllChildren()
 	end)
 
 	afterAll(function()
 		instanceFolder:Destroy()
+		instanceModel:Destroy()
 	end)
 
 	describe("Streamable", function()
@@ -67,6 +82,29 @@ return function()
 			testInstance.Parent = nil
 			task.wait()
 			testInstance.Parent = instanceFolder
+			task.wait()
+			streamable:Destroy()
+			task.wait()
+			expect(observed).to.equal(2)
+			expect(cleaned).to.equal(2)
+		end)
+		
+		it("should detect primary part that is immediately available", function()
+			local testInstance = CreatePrimary()
+			local streamable = Streamable.primary(instanceModel)
+			local observed = 0
+			local cleaned = 0
+			streamable:Observe(function(_instance, trove)
+				observed += 1
+				trove:Add(function()
+					cleaned += 1
+				end)
+			end)
+			task.wait()
+			testInstance.Parent = nil
+			task.wait()
+			testInstance.Parent = instanceModel
+			instanceModel.PrimaryPart = testInstance
 			task.wait()
 			streamable:Destroy()
 			task.wait()
