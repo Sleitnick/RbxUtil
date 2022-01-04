da12a5dab5b03265f77f58b1d0a8cfed0e2323a8
Stephen Leitnick <sleitnick@gmail.com>
2022-01-04 09:11:21 -0500

    New symbol using newproxy

--- modules/symbol/init.lua
+++ modules/symbol/init.lua
@@ -1,81 +1,57 @@
---!strict
-
 -- Symbol
 -- Stephen Leitnick
--- December 27, 2020
-
---[[
-
-	symbol = Symbol.new(id: string [, scope: Symbol])
-
-	Symbol.Is(obj: any): boolean
-	Symbol.IsInScope(obj: any, scope: Symbol): boolean
-
---]]
-
-
-local CLASSNAME = "Symbol"
+-- January 04, 2022
 
 
 --[=[
 	@class Symbol
 
-	Symbols are simply unique objects that can be used as unique identifiers.
-]=]
-local Symbol = {}
-Symbol.__index = Symbol
+	Represents a unique object.
 
---[=[
-	Constructs a new symbol
+	Symbols are often used as unique keys in tables. This is useful to avoid possible collisions
+	with a key in a table, since the symbol will always be unique and cannot be reconstructed.
 
-	@param id any -- Identifier for the symbol (usually a string)
-	@param scope Symbol? -- Optional symbol scope
-	@return boolean -- Returns `true` if the `obj` parameter is a Symbol
+	
+	:::note All Unique
+	Every creation of a symbol is unique, even if the
+	given names are the same.
+	:::
+
+	```lua
+	local Symbol = require(packages.Symbol)
+	
+	-- Create a symbol:
+	local symbol = Symbol("MySymbol")
+
+	-- The name is optional:
+	local anotherSymbol = Symbol()
+
+	-- Comparison:
+	print(symbol == symbol) --> true
+
+	-- All symbol constructions are unique, regardless of the name:
+	print(Symbol("Hello") == Symbol("Hello")) --> false
+
+	-- Commonly used as unique keys in a table:
+	local DATA_KEY = Symbol("Data")
+	local t = {
+		-- Can only be accessed using the DATA_KEY symbol:
+		[DATA_KEY] = {}
+	}
+
+	print(t[DATA_KEY]) --> {}
+	```
 ]=]
-function Symbol.new(id: any, scope: any)
-	assert(id ~= nil, "Symbol ID cannot be nil")
-	if scope ~= nil then
-		assert(Symbol.Is(scope), "Scope must be a Symbol or nil")
+
+local function Symbol(name: string?)
+	local symbol = newproxy(true)
+	if not name then
+		name = ""
 	end
-	local self = setmetatable({
-		ClassName = CLASSNAME;
-		_id = id;
-		_scope = scope;
-	}, Symbol)
-	return self
+	getmetatable(symbol).__tostring = function()
+		return "Symbol(" .. name .. ")"
+	end
+	return symbol
 end
-
-
---[=[
-	Checks if the given object is a Symbol.
-
-	@param obj any -- Anything
-	@return boolean -- Returns `true` if the `obj` parameter is a Symbol
-]=]
-function Symbol.Is(obj: any): boolean
-	return type(obj) == "table" and getmetatable(obj) == Symbol
-end
-
-
-
---[=[
-	Checks if the given object is a Symbol an in the given scope
-
-	@param obj any -- Anything
-	@param scope Symbol -- Scope symbol
-	@return boolean -- Returns `true` if the `obj` parameter is a Symbol and in the given scope
-]=]
-function Symbol.IsInScope(obj: any, scope: Symbol): boolean
-	return Symbol.Is(obj) and obj._scope == scope
-end
-
-
-function Symbol:__tostring()
-	return ("Symbol<%s>"):format(self._id)
-end
-
-
-export type Symbol = typeof(Symbol.new("Test", nil))
-
 
 return Symbol
