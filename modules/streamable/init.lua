7df3b66cc2216ee73e03715937a7d3f145c3ddb8
Rocky28447 <26193486+Rocky28447@users.noreply.github.com>
2022-11-30 22:52:02 -0500

    Strictly typed Streamable
    - Updated Trove dependency to v5.x.x

--- modules/streamable/init.lua
+++ modules/streamable/init.lua
@@ -1,8 +1,34 @@
 -- Streamable
--- Stephen Leitnick
--- November 08, 2021
+-- Rocky28447
+-- November 30th, 2022
+
+--!strict
+
+local StreamableApi: any = require(script.Streamable)
+local StreamableUtilApi: any = require(script.StreamableUtil)
+
+local Signal = require(script.Parent.Signal)
+local Trove = require(script.Parent.Trove)
+
+type Connection = Signal.Connection
+type Signal = Signal.Class
+type Trove = Trove.Class
+
+export type Streamable = {
+	Observe: (self: Streamable, handler: (instance: Instance, trove: Trove) -> ()) -> Connection,
+	Destroy: (self: Streamable) -> (),
+}
 
 return {
-	Streamable = require(script.Streamable),
-	StreamableUtil = require(script.StreamableUtil),
+	Streamable = StreamableApi,
+	StreamableUtil = StreamableUtilApi,
+} :: {
+	Streamable: {
+		new: (parent: Instance, childName: string) -> Streamable,
+		primary: (parent: Model) -> Streamable,
+	},
+
+	StreamableUtil: {
+		Compound: (streamables: { Streamable }, handler: ({ [string]: Instance }, trove: Trove) -> ()) -> Trove,
+	},
 }
