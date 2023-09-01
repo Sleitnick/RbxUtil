09439d0be06eeee482a3c0ba654319f7707169cd
bubshayz <92627259+bubshayz@users.noreply.github.com>
2023-09-01 17:45:20 +0500

    Update init.lua

--- modules/trove/init.lua
+++ modules/trove/init.lua
@@ -4,7 +4,8 @@
 
 local FN_MARKER = newproxy()
 local THREAD_MARKER = newproxy()
-local GENERIC_OBJECT_CLEANUP_METHODS = { "Destroy", "Disconnect", "Cleanup", "Deinit", "Clean", "destroy", "disconnect", "cleanup", "deinit", "clean" }
+local GENERIC_OBJECT_CLEANUP_METHODS =
+	{ "Destroy", "Disconnect", "Cleanup", "Deinit", "Clean", "destroy", "disconnect", "cleanup", "deinit", "clean" }
 
 local RunService = game:GetService("RunService")
 
