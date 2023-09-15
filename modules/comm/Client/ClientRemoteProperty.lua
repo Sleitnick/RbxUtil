dddecd590c034c0e99f18bad3e30d182a9dde26b
Amber Grace <122126644+ambergamefam@users.noreply.github.com>
2023-09-15 12:46:01 -0600

    Do not drop value updates when multiple updates happen same frame

--- modules/comm/Client/ClientRemoteProperty.lua
+++ modules/comm/Client/ClientRemoteProperty.lua
@@ -36,21 +36,26 @@ function ClientRemoteProperty.new(
 )
 	local self = setmetatable({}, ClientRemoteProperty)
 	self._rs = ClientRemoteSignal.new(re, inboundMiddleware, outboudMiddleware)
+
 	self._ready = false
 	self._value = nil
+
 	self.Changed = Signal.new()
-	self._readyPromise = self:OnReady():andThen(function()
-		self._readyPromise = nil
-		self.Changed:Fire(self._value)
-		self._changed = self._rs:Connect(function(value)
-			if value == self._value then
-				return
-			end
-			self._value = value
-			self.Changed:Fire(value)
-		end)
-	end)
 	self._rs:Fire()
+
+	local resolveOnReadyPromise
+	self._readyPromise = Promise.new(function(resolve)
+		resolveOnReadyPromise = resolve
+	end)
+	self._changed = self._rs:Connect(function(value)
+		self._value = value
+		if not self._ready then
+			self._ready = true
+			resolveOnReadyPromise(value)
+		end
+		self.Changed:Fire(value)
+	end)
+
 	return self
 end
 
@@ -86,16 +91,7 @@ end
 	```
 ]=]
 function ClientRemoteProperty:OnReady()
-	if self._ready then
-		return Promise.resolve(self._value)
-	end
-	return Promise.fromEvent(self._rs, function(value)
-		self._value = value
-		self._ready = true
-		return true
-	end):andThen(function()
-		return self._value
-	end)
+	return self._readyPromise
 end
 
 --[=[
