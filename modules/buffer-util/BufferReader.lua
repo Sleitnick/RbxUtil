4474a143233b8f430a1e40e325fc4b91025a7034
Stephen Leitnick <sleitnick@gmail.com>
2023-11-30 17:45:34 -0500

    Buffer util

---
+++ modules/buffer-util/BufferReader.lua
@@ -0,0 +1,217 @@
+--!native
+
+export type BufferReader = {
+	ReadInt8: (self: BufferReader) -> number,
+	ReadUInt8: (self: BufferReader) -> number,
+	ReadInt16: (self: BufferReader) -> number,
+	ReadUInt16: (self: BufferReader) -> number,
+	ReadInt32: (self: BufferReader) -> number,
+	ReadUInt32: (self: BufferReader) -> number,
+	ReadFloat32: (self: BufferReader) -> number,
+	ReadFloat64: (self: BufferReader) -> number,
+	ReadString: (self: BufferReader) -> string,
+	ReadStringRaw: (self: BufferReader, length: number) -> string,
+	GetSize: (self: BufferReader) -> number,
+	ResetCursor: (self: BufferReader) -> (),
+	SetCursor: (self: BufferReader, cursorPosition: number) -> (),
+	GetCursor: (self: BufferReader) -> number,
+	GetBuffer: (self: BufferReader) -> buffer,
+}
+
+local BufferWriter = require(script.Parent.BufferWriter)
+
+--[=[
+	@class BufferReader
+	
+	A BufferReader is an abstraction wrapper for `buffer` objects
+	that provides a convenient way of reading out data from buffers.
+]=]
+local BufferReader = {}
+BufferReader.__index = BufferReader
+
+function BufferReader.new(buf: string | buffer | BufferWriter.BufferWriter): BufferReader
+	if typeof(buf) == "string" then
+		return BufferReader.fromString(buf)
+	elseif typeof(buf) == "buffer" then
+		return BufferReader.fromBuffer(buf)
+	elseif typeof(buf) == "table" and getmetatable(buf :: any) == BufferWriter then
+		return BufferReader.fromBuffer(buf:GetBuffer())
+	end
+
+	error(`expected string or buffer; got {typeof(buf)}`)
+end
+
+function BufferReader.fromBuffer(buf: buffer)
+	local self = setmetatable({
+		_buffer = buf,
+		_size = buffer.len(buf),
+		_cursor = 0,
+	}, BufferReader)
+
+	return self
+end
+
+function BufferReader.fromString(str: string)
+	return BufferReader.fromBuffer(buffer.fromstring(str))
+end
+
+function BufferReader:_assertSize(desiredSize: number)
+	if desiredSize > self._size then
+		error(`cursor out of bounds`, 3)
+	end
+end
+
+--[=[
+	Read a signed 8-bit integer from the buffer.
+]=]
+function BufferReader:ReadInt8(): number
+	self:_assertSize(self._cursor + 1)
+	local n = buffer.readi8(self._buffer, self._cursor)
+	self._cursor += 1
+	return n
+end
+
+--[=[
+	Read an unsigned 8-bit integer from the buffer.
+]=]
+function BufferReader:ReadUInt8(): number
+	self:_assertSize(self._cursor + 1)
+	local n = buffer.readu8(self._buffer, self._cursor)
+	self._cursor += 1
+	return n
+end
+
+--[=[
+	Read a signed 16-bit integer from the buffer.
+]=]
+function BufferReader:ReadInt16(): number
+	self:_assertSize(self._cursor + 2)
+	local n = buffer.readi16(self._buffer, self._cursor)
+	self._cursor += 2
+	return n
+end
+
+--[=[
+	Read an unsigned 16-bit integer from the buffer.
+]=]
+function BufferReader:ReadUInt16(): number
+	self:_assertSize(self._cursor + 2)
+	local n = buffer.readu16(self._buffer, self._cursor)
+	self._cursor += 2
+	return n
+end
+
+--[=[
+	Read a signed 32-bit integer from the buffer.
+]=]
+function BufferReader:ReadInt32(): number
+	self:_assertSize(self._cursor + 4)
+	local n = buffer.readi32(self._buffer, self._cursor)
+	self._cursor += 4
+	return n
+end
+
+--[=[
+	Read an unsigned 32-bit integer from the buffer.
+]=]
+function BufferReader:ReadUInt32(): number
+	self:_assertSize(self._cursor + 4)
+	local n = buffer.readu32(self._buffer, self._cursor)
+	self._cursor += 4
+	return n
+end
+
+--[=[
+	Read a 32-bit single-precision float from the buffer.
+]=]
+function BufferReader:ReadFloat32(): number
+	self:_assertSize(self._cursor + 4)
+	local n = buffer.readf32(self._buffer, self._cursor)
+	self._cursor += 4
+	return n
+end
+
+--[=[
+	Read a 64-bit double-precision float from the buffer.
+]=]
+function BufferReader:ReadFloat64(): number
+	self:_assertSize(self._cursor + 8)
+	local n = buffer.readf64(self._buffer, self._cursor)
+	self._cursor += 8
+	return n
+end
+
+--[=[
+	Read a string from the buffer.
+	
+	:::info
+	This assumes the string was written using the `BufferWriter:WriteString()`
+	method, which stores an extra integer to mark the size of the string.
+]=]
+function BufferReader:ReadString(): string
+	local strLen = self:ReadUInt32()
+	self:_assertSize(self._cursor + strLen)
+	local s = buffer.readstring(self._buffer, self._cursor, strLen)
+	self._cursor += strLen
+	return s
+end
+
+--[=[
+	Read a string from the buffer.
+	
+	:::info
+	This assumes the string was written using the `BufferWriter:WriteStringRaw()`.
+]=]
+function BufferReader:ReadStringRaw(length: number): string
+	length = math.max(0, math.floor(length))
+	self:_assertSize(self._cursor + length)
+	local s = buffer.readstring(self._buffer, self._cursor, length)
+	self._cursor += length
+	return s
+end
+
+--[=[
+	Sets the position of the cursor.
+]=]
+function BufferReader:SetCursor(position: number)
+	position = math.floor(position)
+	if position < 0 or position > self._size then
+		error(`cursor position {position} out of range [0, {self._size}]`, 3)
+	end
+
+	self._cursor = position
+end
+
+--[=[
+	Returns the position of the cursor.
+]=]
+function BufferReader:GetCursor(): number
+	return self._cursor
+end
+
+--[=[
+	Resets the position of the cursor.
+]=]
+function BufferReader:ResetCursor()
+	self._cursor = 0
+end
+
+--[=[
+	Returns the size of the buffer.
+]=]
+function BufferReader:GetSize(): number
+	return self._size
+end
+
+--[=[
+	Returns the `buffer` object.
+]=]
+function BufferReader:GetBuffer(): buffer
+	return self._buffer
+end
+
+function BufferReader:__tostring()
+	return "BufferReader"
+end
+
+return BufferReader
